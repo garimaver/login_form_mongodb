da98e1f78b9a311acf2e5f9f7cab0281f6e2303a
garima verma <114860130+garimaver@users.noreply.github.com>
2023-11-05 13:14:41 +0530

    added css

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 require("./db/connect");
 
 
 const port = process.env.port || 3000;
+
+ const static_path = path.join(__dirname, "../public")
+
+ app.use(express.static(static_path));
+
+
 
 app.get("/", (req, res) => {
     res.send("hello from the garima")
