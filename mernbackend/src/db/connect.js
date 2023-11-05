0c12df497647f45000c15ef7f535751ac56a1b84
garima verma <114860130+garimaver@users.noreply.github.com>
2023-11-05 13:02:24 +0530

    added mongoose

--- mernbackend/src/db/connect.js
+++ mernbackend/src/db/connect.js
@@ -6,4 +6,6 @@ mongoose.connect("mongodb://localhost:27017/mernRegisteration",{
     useCreateIndex:true
 }).then(()=> {
     console.log(`connection successful`);
+}).catch((e)=> {
+    console.log(`no connection`);
 })
