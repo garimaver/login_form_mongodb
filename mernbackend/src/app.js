cf75ca82fd8460b9a694d5825069e044822231c0
garima verma <114860130+garimaver@users.noreply.github.com>
2023-11-06 11:16:43 +0530

    app js file

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
+
+
 require("./db/connect");
+const Register = require("./models/registers");
 
 
 const port = process.env.port || 3000;
@@ -10,6 +13,8 @@ const port = process.env.port || 3000;
  const static_path = path.join(__dirname, "../public")
  const templates_path = path.join(__dirname, "../templates/views");
  const partials_path = path.join(__dirname, "../templates/partials");
+
+app.use(express.json());
 
  app.use(express.static(static_path));
  app.set("view engine", "hbs")
@@ -25,6 +30,16 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
 res.render("register");
 })
+
+//create a new user in our database
+app.post("/register", async (req, res) => {
+    try {
+        console.log(req.body.firstname);
+        res.send(req.body)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    })
 
 app.listen(port, () => {
 console.log(`server is running at port on ${port}`);
--- mernbackend/src/models/registers.js
+++ mernbackend/src/models/registers.js
@@ -1,1 +1,44 @@
-const  
+const   mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+firstname : {
+    type:String,
+    required : true
+},
+lastname : {
+    type:String,
+    required : true
+},
+email : {
+    type:String,
+    required : true,
+    unique:true
+}, 
+gender : {
+    type:String,
+    required : true
+},
+phone : {
+    type:Number,
+    required : true,
+    unique:true
+},
+age: {
+    type:Number,
+    required : true
+},
+password: {
+    type:String,
+    required : true
+},
+confirmpassword : {
+    type:String,
+    required : true
+}
+})
+
+// now we need to create a collections
+
+const register = new mongoose.model("Register", employeeSchema);
+
+module.exports = register;
