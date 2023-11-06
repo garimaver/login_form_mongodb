ab8a9a1681469e18d35cb2c019b5b53aab6c4476
garima verma <114860130+garimaver@users.noreply.github.com>
2023-11-06 12:51:05 +0530

    added  login hbs

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -15,6 +15,7 @@ const port = process.env.port || 3000;
  const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
  app.use(express.static(static_path));
  app.set("view engine", "hbs")
@@ -31,11 +32,35 @@ app.get("/register", (req, res) => {
 res.render("register");
 })
 
+app.get("/login", (req, res) => {
+    res.render("login");
+    })
+
+
+
 //create a new user in our database
 app.post("/register", async (req, res) => {
     try {
-        console.log(req.body.firstname);
-        res.send(req.body)
+       const password = req.body.password;
+       const cpassword = req.body.confirmpassword;
+
+       if(password == cpassword){
+      const registerEmployee = new Register({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            gender: req.body.gender,
+            phone: req.body.phone,
+            age: req.body.age,
+            password: password,
+            confirmpassword:cpassword
+      })
+
+      const registered = await registerEmployee.save();
+      res.status(201).render("index");
+       }else{
+        res.send("password are not matching")
+       }
     } catch (error) {
         res.status(400).send(error)
     }
